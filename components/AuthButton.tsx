32a8e07bf8896f13748f32718818b860c5acd380
dnetadmin <admin@decenternet.com>
2024-04-19 00:25:00 +0000

    Removed signout from auth.

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,9 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-// components/AuthButton.tsx
 import { signOut } from "@/utils/supabase/actions";
-
 
 export default async function AuthButton() {
   const supabase = createClient();
@@ -11,14 +9,6 @@ export default async function AuthButton() {
   const {
     data: { user },
   } = await supabase.auth.getUser();
-
-  const signOut = async () => {
-    "use server";
-
-    const supabase = createClient();
-    await supabase.auth.signOut();
-    return redirect("/login");
-  };
 
   return user ? (
     <div className="flex items-center gap-4">
